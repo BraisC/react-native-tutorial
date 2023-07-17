4a553f740c941154e359c01d0d9dd398d7fb3134
BraisC <braiscao.dev@gmail.com>
2023-07-17 23:18:35 +0200

    02.02-Exploring Core Components

--- RNCourse/.eslintrc.js
+++ RNCourse/.eslintrc.js
@@ -3,5 +3,12 @@ module.exports = {
   rules: {
     'react/style-prop-object': 0,
     '@typescript-eslint/no-use-before-define': 0,
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: ['function-declaration', 'function-expression', 'arrow-function'],
+        unnamedComponents: 'function-expression',
+      },
+    ],
   },
 };
