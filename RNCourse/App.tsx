0c369933915b74888eff0cd538a5b23feedea2b3
BraisC <braiscao.dev@gmail.com>
2023-07-17 23:24:38 +0200

    02.003 - Working with Core Components

--- RNCourse/App.tsx
+++ RNCourse/App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const App = () => (
   <View style={styles.container}>
+    <View>
+      <Text>Another piece of text</Text>
+    </View>
     <Text>Hello World</Text>
-    <StatusBar style="auto" />
+    <Button title="Tap me" />
   </View>
 );
 
